cb23442df58ec53627078aeeb3b1b8e7a0ae5702
stokato <demyanovpaul@yandex.ru>
2019-02-21 00:24:58 +0300

    My first app to SpringBoot and React

---
+++ src/main/java/tutorial/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package tutorial;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+    @Override
+    void delete(Long aLong);
+}
